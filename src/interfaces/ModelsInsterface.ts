e6ad81076df822b761a34c3894d5d1771e00e3bf
everton-canez <everton-canez@procergs.rs.gov.br>
2019-11-21 11:11:52 -0300

    Models e interfaces

--- src/interfaces/ModelsInsterface.ts
+++ src/interfaces/ModelsInsterface.ts
@@ -1,3 +1,9 @@
+import { CommentModel } from './../models/CommentModel';
+import { PostModel } from './../models/PostAttributes';
+import { UserModel } from './../models/UserModel';
+
 export interface ModelsInterface {
-    
+    User: UserModel
+    Post: PostModel
+    Comment: CommentModel
 }
